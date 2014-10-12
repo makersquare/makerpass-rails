6d46cd111d0223e9559e98b1e29d569257e7ca32
Kaoru Kohashigawa <kaoru@spredfast.com>
2014-10-12 16:56:53 -0500

    Setup routes and default sessions actions

--- app/controllers/maker_pass/sessions_controller.rb
+++ app/controllers/maker_pass/sessions_controller.rb
@@ -1,10 +1,17 @@
 module MakerPass
   class SessionsController < ApplicationController
     def create
-      raise "it worked, yo"
+      auth_hash = request.env['omniauth.auth']
+
+      user = User.find_or_create_by_auth_hash(auth_hash)
+      session[:user_id] = user.id
+
+      redirect_to (session.delete(:return_to) || main_app.root_path)
     end
 
     def destroy
+      reset_session
+      redirect_to root_path
     end
   end
 end
