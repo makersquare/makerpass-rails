5e34fc1541f20c98d6f7e4f8087050d8661e4323
Kaoru Kohashigawa <kaoru@spredfast.com>
2014-10-12 17:43:27 -0500

    Allow for custom sign_in and sign_out routes

--- app/controllers/maker_pass/sessions_controller.rb
+++ app/controllers/maker_pass/sessions_controller.rb
@@ -6,12 +6,25 @@ module MakerPass
       user = User.find_or_create_by_auth_hash(auth_hash)
       session[:user_id] = user.id
 
-      redirect_to (session.delete(:return_to) || main_app.root_path)
+      redirect_to find_redirect_path(:after_sign_in_path)
     end
 
     def destroy
       reset_session
-      redirect_to main_app.root_path
+      redirect_to find_redirect_path(:after_sign_out_path)
     end
+
+    def find_redirect_path(path)
+      if session[:return_to]
+        session.delete(:return_to)
+      elsif main_app.respond_to? path
+        main_app.public_send(path)
+      elsif main_app.respond_to? :root_path
+        main_app.root_path
+      else
+        "/"
+      end
+    end
+
   end
 end
