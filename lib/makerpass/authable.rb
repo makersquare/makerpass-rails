1b762858f1952cb0106a40e456bf29b475cd260a
Kaoru Kohashigawa <kaoru@spredfast.com>
2014-10-12 17:57:07 -0500

    Write method on model when authable is included

--- lib/makerpass/authable.rb
+++ lib/makerpass/authable.rb
@@ -1,7 +1,20 @@
 module MakerPass
   module Authable
     def self.included(base)
+      base.extend(ClassMethods)
+    end
 
+    module ClassMethods
+      def find_or_create_by_auth_hash(auth)
+        user = self.find_or_create_by(uid: auth['uid'])
+
+        user.name = auth['info']['name']
+        user.email = auth['info']['email']
+        user.avatar_url = auth['info']['avatar_url']
+        user.save if user.changed?
+
+        user
+      end
     end
   end
 end
